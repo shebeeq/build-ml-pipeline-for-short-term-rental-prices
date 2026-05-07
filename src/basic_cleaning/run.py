748315e4a2693d59ff4499c5459b088001660a0b
Shebeeq Arakkaveetil Aboobakar <shebeeq.arakkaveetil.aboobakar@cariad.technology>
2026-05-07 14:38:35 +0200

    Include wandb_utils in basic_cleaning for portability

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import wandb
 import pandas as pd
+from wandb_utils.log_artifact import log_artifact
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
